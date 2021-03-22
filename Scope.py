87598cc9763f0d47756ef86fb6bbc4dd938be4c5
jideabdqudus <jideabdqudus@gmail.com>
2021-03-22 16:25:01 +0100

    Black Jack Game

---
+++ Scope.py
@@ -0,0 +1,20 @@
+############SCOPE##############
+
+"""
+enemies = 1  #Global Variable
+
+
+def increase_enemies():
+    global enemies
+    enemies += 1     #Local Variable
+    print(f"Enemies inside function is now {enemies}")
+
+
+increase_enemies()
+print(f"Enemies from outside function is now {enemies}")
+"""
+
+def numberGuess():
+    print("Welcome to the Number Guessing Game")
+    print("I'm thinking of a number between 1 and 100")
+

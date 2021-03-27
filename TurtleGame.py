51c80bba6b8f7ce4dac78e6a65eead341c71b924
jideabdqudus <jideabdqudus@gmail.com>
2021-03-27 20:00:45 +0100

    Turtle Game

--- TurtleGame.py
+++ TurtleGame.py
@@ -1,44 +1,44 @@
 from turtle import Turtle, Screen, colormode
 from random import choice, randint
 
-# kags = Turtle()
-# print(kags)
-# kags.shape("turtle")
-# kags.color("blue", "yellow")
-# kags.forward(100)
-# kags.right(90)
-# kags.forward(100)
-# kags.right(90)
-# kags.forward(100)
-# kags.right(90)
-# kags.forward(100)
-#
-# my_screen = Screen()
-# print(my_screen.canvheight)
-# my_screen.exitonclick()
+kags = Turtle()
+print(kags)
+kags.shape("turtle")
+kags.color("blue", "yellow")
+kags.forward(100)
+kags.right(90)
+kags.forward(100)
+kags.right(90)
+kags.forward(100)
+kags.right(90)
+kags.forward(100)
 
-# turtle = Turtle()
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
+
+turtle = Turtle()
 colormode(255)
-#
-# colors = ["red", "SeaGreen", "wheat", "yellow", "black", "purple", "IndianRed", "DeepSkyBlue", "SlateGray"]
-# directions = [0, 90, 180, 270]
-#
-# for _ in range(200):
-#     turtle.shape("turtle")
-#     turtle.width(10)
-#     turtle.speed("fast")
-#     random_int = randint(1, 255)
-#     random_int2 = randint(1, 255)
-#     random_int3 = randint(1, 255)
-#     tup = (random_int, random_int2, random_int3)
-#     turtle.pencolor(tup)
-#     # turtle.color(choice(colors))
-#     turtle.forward(30)
-#     turtle.setheading(choice(directions))
-#
-#
-# screen = Screen()
-# screen.exitonclick()
+
+colors = ["red", "SeaGreen", "wheat", "yellow", "black", "purple", "IndianRed", "DeepSkyBlue", "SlateGray"]
+directions = [0, 90, 180, 270]
+
+for _ in range(200):
+    turtle.shape("turtle")
+    turtle.width(10)
+    turtle.speed("fast")
+    random_int = randint(1, 255)
+    random_int2 = randint(1, 255)
+    random_int3 = randint(1, 255)
+    tup = (random_int, random_int2, random_int3)
+    turtle.pencolor(tup)
+    # turtle.color(choice(colors))
+    turtle.forward(30)
+    turtle.setheading(choice(directions))
+
+
+screen = Screen()
+screen.exitonclick()
 
 
 turtle = Turtle()

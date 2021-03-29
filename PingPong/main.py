b245eb5203e4b8efecafd27bbaac2e6bf62c3785
jideabdqudus <jideabdqudus@gmail.com>
2021-03-29 23:04:36 +0100

    Ping Pong.

---
+++ PingPong/main.py
@@ -0,0 +1,8 @@
+# Create the screen
+# Create and move a paddle
+# Create another paddle
+# Create a ball and move it
+# Detect collision with wall and bounce
+# Detect collision with paddle
+# Detect when paddle misses
+# Keep score

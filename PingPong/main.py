f6b6dfbc7c3b2546d125d4d48a0faa7d4b1fb403
jideabdqudus <jideabdqudus@gmail.com>
2021-03-30 11:01:01 +0100

    Ping Pong

--- PingPong/main.py
+++ PingPong/main.py
@@ -1,3 +1,31 @@
+from turtle import Screen
+from paddle import Paddle
+
+screen = Screen()
+paddle = Paddle()
+
+screen.setup(height=600, width=800)
+screen.bgcolor("black")
+screen.title("Ping Pong")
+screen.exitonclick()
+screen.tracer(0)
+screen.listen()
+
+screen.onkey(fun=paddle.move_paddle_up, key="Up")
+screen.onkey(fun=paddle.move_paddle_down, key="Down")
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Create the screen
 # Create and move a paddle
 # Create another paddle
@@ -6,3 +34,4 @@
 # Detect collision with paddle
 # Detect when paddle misses
 # Keep score
+
---
+++ PingPong/paddle.py
@@ -0,0 +1,24 @@
+from turtle import Turtle
+
+
+class Paddle(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.create_paddle()
+
+    def create_paddle(self):
+        self.shape("square")
+        self.penup()
+        self.color("white")
+        self.goto(x=350, y=0)
+        self.turtlesize(stretch_wid=5, stretch_len=1)
+
+    def move_paddle_up(self):
+        print("working")
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def move_paddle_down(self):
+        print("working")
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)

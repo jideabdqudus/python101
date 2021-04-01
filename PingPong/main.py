98a28eb872df3982da32db78a40141817e0d178f
jideabdqudus <jideabdqudus@gmail.com>
2021-04-01 20:50:01 +0100

    Ping Pong

--- PingPong/main.py
+++ PingPong/main.py
@@ -35,11 +35,6 @@ while game_is_on:
 
 screen.exitonclick()
 
-# Create the screen
-# Create and move a paddle
-# Create another paddle
-# Create a ball and move it
-# Detect collision with wall and bounce
 # Detect collision with paddle
 # Detect when paddle misses
 # Keep score

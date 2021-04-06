a77e82a2be2c70ebc0bb04687c28374c1f7f84d3
jideabdqudus <jideabdqudus@gmail.com>
2021-04-06 13:39:36 +0100

    Unit Testing

--- Main.py
+++ Main.py
@@ -91,21 +91,27 @@ for x in fib(20):
 
 import random
 
-def guessing_game():
-    answer = random.randint(1, 10)
-    started = True
+
+started = True
+
+def guessing_game(guess, answer):
+    if 0 < guess < 11:
+        if guess == answer:
+            print("You're a genius")
+            return True
+    else:
+        print("Hey Bozo, I said 1-10")
+        return False
+
+
+answer = random.randint(1, 10)
+
+if __name__ == '__main__':
     while started:
         try:
             guess = int(input("Guess a number 1-10: "))
-            if 0 < guess < 11:
-                if guess == answer:
-                    print("You're a genius")
-                    started = False
-            else:
-                print("Hey Bozo, I said 1-10")
-
+            if guessing_game(guess, answer):
+                break
         except ValueError:
             print("Please enter a number")
             continue
-
-guessing_game()
--- test.py
+++ test.py
@@ -6,8 +6,21 @@ class TestCase(unittest.TestCase):
         print("About to run a Test Function")
 
     def testcase1(self):
-        result = guessing_game()
-        self.assertEqual()
+        result = guessing_game(5, 5)
+        self.assertTrue(result)
 
-        if __name__ == '__main__':
-            unittest.main()
+    def testcase2(self):
+        result = guessing_game(5, 0)
+        self.assertFalse(result)
+
+    def testcase3(self):
+        result = guessing_game(5, 11)
+        self.assertFalse(result)
+
+    def testcase4(self):
+        result = guessing_game(5, "aksks")
+        self.assertFalse(result)
+
+
+if __name__ == '__main__':
+   unittest.main()
